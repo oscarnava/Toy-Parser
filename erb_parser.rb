9326240bc18b3e730e0447a8f63da00c6e7d5c90
oscarnava <oscarnava@gmail.com>
2019-09-10 12:16:37 -0500

    Created object with nested parsing capabilities

--- erb_parser.rb
+++ erb_parser.rb
@@ -1,18 +1,52 @@
-def parse(template)
-  context = binding
-  input = File.read(template)
-  html = input.to_s
-  html.gsub(/<%([=#])?(.*)%>/) do
-    case $1
-    when '='
-      eval($2, context)
-    when '#'
-      ''
+class ERBParser
+
+  attr_accessor :contents
+
+  def initialize(template)
+    @contents = if template.end_with? '.html.erb'
+      File.read(template).to_s
     else
-      eval($2, context)
-      ''
+      template
     end
-  end #.gsub(/>\s+</,'><')
+    @subtemplate = nil
+  end
+
+  def parse
+    context = binding
+    @contents.gsub!(/<%([=#])?(.*)%>/) do
+      cmd = $2.strip
+      case $1
+      when '='
+        eval(cmd, context)
+      when '#'
+        ''
+      else
+        eval(cmd, context)
+        ''
+      end
+    end #.gsub(/>\s+</,'><')
+    @contents
+  end
+
+  def <<(template)
+    if @subtemplate
+      @subtemplate << template
+    else
+      @subtemplate = ERBParser.new(template)
+    end
+    self
+  end
+
+  def to_s
+    parse {
+      if @subtemplate
+        @subtemplate.to_s
+      else
+        '<!-- Nothing to yield! -->'
+      end
+    }
+  end
+
 end
 
-puts parse('master.html.erb') { parse('home.html.erb') }
+puts ERBParser.new('master.html.erb') << 'template.html.erb' << 'subtemplate.html.erb'
